0649bf08dd7387187182972e1af26270e7d265c9
honoriothovela <honoriothovela@gmail.com>
2025-08-19 19:16:32 +0200

    goon

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,45 +2,33 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# Buscar o security group existente
+data "aws_security_group" "existing_ssh" {
+  filter {
+    name   = "group-name"
+    values = ["allow_ssh"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-020741d6f0bbd3b0d"] # Substitua pelo ID da sua VPC
+  }
+}
+
 resource "aws_instance" "my_instance" {
-  ami           = "ami-042b4708b1d05f512"  # Ubuntu 22.04 em eu-north-1
+  ami           = "ami-042b4708b1d05f512"
   instance_type = "t3.micro"
   key_name      = "my-key_pair"
 
-  # Adicionando security group para permitir SSH
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  # Usar o security group existente
+  vpc_security_group_ids = [data.aws_security_group.existing_ssh.id]
 
   tags = {
     Name = "k3s-ec2-machine"
   }
 }
 
-# Security group para permitir SSH
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
 output "ec2_public_ip" {
   value       = aws_instance.my_instance.public_ip
   description = "IP público da instância EC2"
-  sensitive   = false  # Garantir que não seja marcado como sensitive
 }
