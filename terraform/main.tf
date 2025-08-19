422f6d953981670d63165b27934aacd48b081c65
honoriothovela <honoriothovela@gmail.com>
2025-08-19 18:00:54 +0200

    teste_again5

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "k3s_server" {
   ami           = "ami-042b4708b1d05f512"
   instance_type = "t3.micro"
-  key_name      = "my-key_pair" # substitua pelo nome da key pair na AWS
+  key_name      = "my-key_pair" # substitua pelo nome da sua key pair na AWS
 
   tags = {
     Name = "k3s-ec2-machine"
   }
 }
 
-output "ec2_public_ip" {
-  value       = aws_instance.my_instance.public_ip
+output "instance_public_ip" {
+  value       = aws_instance.k3s_server.public_ip
   description = "IP público da instância EC2"
 }
