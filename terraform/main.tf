ac95debefa4c5e075281b304d64a9fe4e8e467a2
honoriothovela <honoriothovela@gmail.com>
2025-08-19 17:29:20 +0200

    teste6

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,14 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-042b4708b1d05f512"
   instance_type = "t3.micro"
+  key_name      = "my-key_pair" # substitua pelo nome da key pair na AWS
+
   tags = {
     Name = "k3s-ec2-machine"
   }
 }
 
+output "ec2_public_ip" {
+  value       = aws_instance.my_instance.public_ip
+  description = "IP público da instância EC2"
+}
